4a3e27115aef9fc6f4c888d8a5e0fdec31717a45
Ken Jiang <kjiang2000@587234276.guest.umich.net>
2018-10-13 09:04:47 -0400

    nt end

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+
+new Vue({
+  el: '#app',
+  components: {
+    'app': App
+  }
+})
